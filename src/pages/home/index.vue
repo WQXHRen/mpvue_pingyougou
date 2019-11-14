e0c36f58990601e234b53fdb09251ae630729c93
无情小黄人 <lipei729744115@163.com>
2019-11-14 17:36:26 +0800

    搜索条+轮播图静态完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,11 +4,25 @@
       <div class="searchBar">
         <icon
           type="search"
-          size="24">
+          size="20">
         </icon>
         <span class="search">搜索</span>
       </div>
     </header>
+
+    <!-- 轮播图 -->
+    <swiper indicator-dots autoplay circular indicator-color="rgba(255,255,255,.3)"  indicator-active-color="rgba(255,255,255)">
+        <block v-for="item in 3" :key="item">
+            <swiper-item>
+                <image mode="aspectFill" src="/static/images/test.png"></image>
+            </swiper-item>
+        </block>
+    </swiper>
+
+    <!-- 分类 -->
+    <div class="category">
+      
+    </div>
 
   </div>
 </template>
@@ -19,6 +33,32 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less'>
+  header {
+    height: 100rpx;
+    background-color: #eb4450;
+    padding: 0 16rpx ;
+    display: flex;
+    align-items: center;
+    .searchBar{
+      width: 100%;
+      height: 60rpx;
+      border-radius: 8rpx;
+      background-color: #ffffff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #bbb;
+      .search{
+        margin-left: 16rpx;
+      }
+    }
+  }
 
+  swiper{
+    image{
+      width: 100%;
+      height: 340rpx;
+    }
+  }
 </style>
